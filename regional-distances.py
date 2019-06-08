8cbdfacbfbdd20f0affd67a5e079aa45bb96d117
georgis8581 <50933361+georgis8581@users.noreply.github.com>
2019-06-08 13:59:11 +0100

    Update regional-distances.py

--- regional-distances.py
+++ regional-distances.py
@@ -1,15 +1,19 @@
+#using this code to automatically find the distances in km from all regions to all regions and the store them in an excel sheet using the direction API from google used
 import googlemaps
 import pandas as pd
 import xlsxwriter
 
+#creating the dataframe to read from my excel sheet 'name_of_regions'
 excel = pd.ExcelFile(r'C:/Users/georg/Documents/Transport and Business Management - Imperial College London/Dissertation/Orginiser.xlsx')
 df = excel.parse("name_of_regions")
 
+#converted the values to a list instead of a table
 origin = df['Name of Regions'].values.tolist()
 destination = df['Name of Regions'].values.tolist()
 
 gmaps = googlemaps.Client(key="AIzaSyDKs_6N5z4_dzYSafEWIiZRSejQJIGGx7g")
 
+#created an excel file name 'test' and stored there the output
 workbook = xlsxwriter.Workbook('test.xlsx')
 worksheet = workbook.add_worksheet()
 for i in range(len(origin)):
@@ -20,5 +24,4 @@ for i in range(len(origin)):
 		dur = distances[j]['rows'][0]['elements'][0]['duration']['text']
 		print(j)
 		worksheet.write(j, i, dist)
-		#worksheet.write(i, j + 1, dur)
 workbook.close()
